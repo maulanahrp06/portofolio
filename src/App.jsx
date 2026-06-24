66b2abfc4ca0a64b5288a8822b2ba3b0d50d1682
Maulana Iksan <maulanaihsan281993@gmail.com>
2026-06-24 22:17:22 +0700

    update: perbaiki durasi Lenis

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
 
   useEffect(() => {
     const lenis = new Lenis({
-      duration: 1.0,
+      duration: 1.1,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
       smoothWheel: true,
     });
